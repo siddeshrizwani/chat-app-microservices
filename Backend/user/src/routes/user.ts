f7c3b80e137588fa6b268f04cfa22ec7ac7ca0e8
Siddesh Rizwani <siddeshrizwani@gmail.com>
2026-08-03 00:02:17 +0530

    new user controller for updating name of the user , find all users , find user by id

--- Backend/user/src/routes/user.ts
+++ Backend/user/src/routes/user.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import { loginUser, verifyUser, myProfile } from "../controllers/user.js"
+import { loginUser, verifyUser, myProfile, updateName, getAllUsers, getAUser } from "../controllers/user.js"
 import { isAuth } from "../middleware/isAuth.js"
 
 const router = express.Router()
@@ -7,5 +7,8 @@ const router = express.Router()
 router.post("/login", loginUser)
 router.post("/verify", verifyUser)
 router.get("/me", isAuth, myProfile)
+router.put("/update/user", isAuth, updateName)
+router.get("/user/all", isAuth, getAllUsers)
+router.get("/user/:id", getAUser)
 
 export default router
